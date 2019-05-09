38b563fe02ba96a340baf3dabefda9114da89872
Richard Hpa <richard.hpa@hotmail.com>
2019-05-10 11:55:01 +1200

    added objects list

--- Week2/Friday/js/script.js
+++ Week2/Friday/js/script.js
@@ -66,15 +66,15 @@ var car = {
     warrent: false,
     owners: ['Sam', 'Jack', 'Jess']
 }
-console.log(car);
-console.log(car.year);
-console.log(car['year']);
-car.year = 2019;
-console.log(car);
-console.log(car.year);
-
-car.km = 10000;
-console.log(car);
+// console.log(car);
+// console.log(car.year);
+// console.log(car['year']);
+// car.year = 2019;
+// console.log(car);
+// console.log(car.year);
+//
+// car.km = 10000;
+// console.log(car);
 
 var items = [
     {
@@ -95,7 +95,7 @@ var items = [
 ];
 
 for (var i = 0; i < items.length; i++) {
-    console.log(items[i]);
+    // console.log(items[i]);
     if(items[i].inStock === true){
         document.getElementById('list').innerHTML += '<li>' + items[i].name + ' - $' +  items[i].price+'</li>';
     }
@@ -104,7 +104,24 @@ for (var i = 0; i < items.length; i++) {
 
 var days = [
     {
-        day: 'monday',
-        tasks: ['cleaning', 'shopping']
+        day: 'Monday',
+        tasks: ['Cleaning', 'Shopping']
+    },
+    {
+        day: 'Tuesday',
+        tasks: ['Homework']
+    },
+    {
+        day: 'Wednesday',
+        tasks: ['Work', 'Gym', 'Take out Rubbish']
     }
-]
+];
+
+for (var i = 0; i < days.length; i++) {
+    console.log(days[i].day);
+    // console.log(days[i].tasks);
+    for (var a = 0; a < days[i].tasks.length; a++) {
+        console.log(days[i].tasks[a]);
+    }
+
+}
